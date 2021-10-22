b641dbaba189bbc73df1701c84cfa43fb57b00b1
Luis M Canaval S <public02@canaval.org>
2021-10-22 12:53:20 -0500

    close #2

--- graph_algorithms.py
+++ graph_algorithms.py
@@ -14,3 +14,20 @@ def bfs(G, s):
         queue.append(v)
 
   return parent
+
+
+def dfs(G, s):
+  n = len(G)
+  visited = [False]*n
+  parent = [None]*n
+
+  def _dfs(u):
+    visited[u] = True
+    for v in G[u]:
+      if not visited[v]:
+        parent[v] = u
+        _dfs(v)
+
+  _dfs(s)
+
+  return parent
